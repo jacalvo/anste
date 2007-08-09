4595629f9e8d4fd9021056ea65994de895aea828
José Antonio Calvo <jacalvo@zentyal.com>
2007-08-09 16:38:47 +0000

    Added default values for options not present in the configuration file. refers #9
    git-svn-id: https://pubsvn.warp.es/anste/trunk@43 18403268-a619-4aed-af80-3a7c80afbb3a

--- src/lib/ANSTE/Config.pm
+++ src/lib/ANSTE/Config.pm
@@ -24,7 +24,6 @@ use constant CONFIG_FILE => 'data/anste.conf';
 
 my $singleton;
 
-# TODO: Return default values in case of undef
 # TODO: Add exception throwing when incorrect values in the file
 
 sub instance 
@@ -36,6 +35,8 @@ sub instance
         $self->{config} = Config::Tiny->read(CONFIG_FILE);
         
         $singleton = bless($self, $class);
+
+        $singleton->_setDefaults();
     }
 
     return $singleton;
@@ -45,154 +46,204 @@ sub system
 {
     my ($self) = @_;
 
-    return $self->{config}->{'global'}->{'system'};
+    return $self->_getOption('global', 'system');
 }
 
 sub virtualizer
 {
     my ($self) = @_;
 
-    return $self->{config}->{'global'}->{'virtualizer'};
+    return $self->_getOption('global', 'virtualizer');
 }
 
 sub imagePath
 {
     my ($self) = @_;
 
-    return $self->{config}->{'paths'}->{'images'};
+    return $self->_getOption('paths', 'images');
 }
 
 sub imageTypePath
 {
     my ($self) = @_;
 
-    return $self->{config}->{'paths'}->{'image-types'};
+    return $self->_getOption('paths', 'image-types');
 }
 
 sub scenarioPath
 {
     my ($self) = @_;
 
-    return $self->{config}->{'paths'}->{'scenarios'};
+    return $self->_getOption('paths', 'scenarios');
 }
 
 sub profilePath
 {
     my ($self) = @_;
 
-    return $self->{config}->{'paths'}->{'profiles'};
+    return $self->_getOption('paths', 'profiles');
 }
 
 sub scriptPath
 {
     my ($self) = @_;
 
-    return $self->{config}->{'paths'}->{'scripts'};
+    return $self->_getOption('paths', 'scripts');
 }
 
 sub anstedPort
 {
     my ($self) = @_;
 
-    return $self->{config}->{'ansted'}->{'port'};
+    return $self->_getOption('ansted', 'port');
 }
 
 sub masterPort
 {
     my ($self) = @_;
 
-    return $self->{config}->{'master'}->{'port'};
+    return $self->_getOption('master', 'port');
 }
 
 sub ipRange
 {
     my ($self) = @_;
 
-    return $self->{config}->{'comm'}->{'ip-range'};
+    return $self->_getOption('comm', 'ip-range');
 }
 
 sub gateway
 {
     my ($self) = @_;
 
-    return $self->{config}->{'comm'}->{'gateway'};
+    return $self->_getOption('comm', 'gateway');
 }
 
 sub xenDir
 {
     my ($self) = @_;
 
-    return $self->{config}->{'xen-options'}->{'dir'};
+    return $self->_getOption('xen-options', 'dir');
 }
 
 sub xenInstallMethod
 {
     my ($self) = @_;
 
-    return $self->{config}->{'xen-options'}->{'install-method'};
+    return $self->_getOption('xen-options', 'install-method');
 }
 
 sub xenSize
 {
     my ($self) = @_;
 
-    return $self->{config}->{'xen-options'}->{'size'};
+    return $self->_getOption('xen-options', 'size');
 }
 
 sub xenMemory
 {
     my ($self) = @_;
 
-    return $self->{config}->{'xen-options'}->{'memory'};
+    return $self->_getOption('xen-options', 'memory');
 }
 
 sub xenNoSwap
 {
     my ($self) = @_;
 
-    return $self->{config}->{'xen-options'}->{'noswap'};
+    return $self->_getOption('xen-options', 'noswap');
 }
 
 sub xenFS
 {
     my ($self) = @_;
 
-    return $self->{config}->{'xen-options'}->{'fs'};
+    return $self->_getOption('xen-options', 'fs');
 }
 
 sub xenDist
 {
     my ($self) = @_;
 
-    return $self->{config}->{'xen-options'}->{'dist'};
+    return $self->_getOption('xen-options', 'dist');
 }
 
 sub xenImage
 {
     my ($self) = @_;
 
-    return $self->{config}->{'xen-options'}->{'image'};
+    return $self->_getOption('xen-options', 'image');
 }
 
 sub xenKernel
 {
     my ($self) = @_;
 
-    return $self->{config}->{'xen-options'}->{'kernel'};
+    return $self->_getOption('xen-options', 'kernel');
 }
 
 sub xenInitrd
 {
     my ($self) = @_;
 
-    return $self->{config}->{'xen-options'}->{'initrd'};
+    return $self->_getOption('xen-options', 'initrd');
 }
 
 sub xenMirror
 {
     my ($self) = @_;
 
-    return $self->{config}->{'xen-options'}->{'mirror'};
+    return $self->_getOption('xen-options', 'mirror');
+}
+
+sub _getOption # (section, option)
+{
+    my ($self, $section, $option) = @_;
+
+    my $config = $self->{config}->{$section}->{$option};
+
+    # TODO: Manage overriden options (by passing parameters, etc)
+    if (defined $config) {
+        return $config;
+    } else {
+        return $self->{default}->{$section}->{$option};
+    }
+}
+
+sub _setDefaults
+{
+    my ($self) = @_;
+
+    $self->{default}->{'global'}->{'system'} = 'Debian';
+    $self->{default}->{'global'}->{'virtualizer'} = 'Xen';
+
+    $self->{default}->{'paths'}->{'images'} = '/home/xen/domains';
+    $self->{default}->{'paths'}->{'image-types'} = 'data/images';
+    $self->{default}->{'paths'}->{'scenarios'} = 'data/scenarios';
+    $self->{default}->{'paths'}->{'profiles'} = 'data/profiles';
+    $self->{default}->{'paths'}->{'scripts'} = 'data/scripts';
+
+    $self->{default}->{'ansted'}->{'port'} = '8000';
+
+    $self->{default}->{'master'}->{'port'} = '8001';
+
+    $self->{default}->{'comm'}->{'ip-range'} = '192.168.0';
+    $self->{default}->{'comm'}->{'gateway'} = '192.168.0.1';
+
+    $self->{default}->{'xen-options'}->{'dir'} = '/home/xen';
+    $self->{default}->{'xen-options'}->{'install-method'} = 'debootstrap';
+    $self->{default}->{'xen-options'}->{'size'} = '2Gb';
+    $self->{default}->{'xen-options'}->{'memory'} = '512Mb';
+    $self->{default}->{'xen-options'}->{'noswap'} = '1';
+    $self->{default}->{'xen-options'}->{'fs'} = 'ext3';
+    $self->{default}->{'xen-options'}->{'dist'} = 'sarge';
+    $self->{default}->{'xen-options'}->{'image'} = 'full';
+    $self->{default}->{'xen-options'}->{'kernel'} = 
+        '/boot/vmlinuz-`uname -r`';
+    $self->{default}->{'xen-options'}->{'initrd'} = 
+        '/boot/initrd.img-`uname-r`';
+    $self->{default}->{'xen-options'}->{'mirror'} =
+        'http://ftp.debian.org/debian';
 }
 
 1;
