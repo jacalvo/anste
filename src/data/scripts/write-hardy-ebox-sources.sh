3ba9a336445f28c76c2195432e789022f87e7555
José Antonio Calvo <jacalvo@zentyal.com>
2009-04-02 13:27:00 +0000

    add line for ebox repository on the local machine
    git-svn-id: https://pubsvn.warp.es/anste/trunk@357 18403268-a619-4aed-af80-3a7c80afbb3a

--- src/data/scripts/write-hardy-ebox-sources.sh
+++ src/data/scripts/write-hardy-ebox-sources.sh
@@ -6,4 +6,5 @@ if ! grep -q ebox-unstable $SOURCES
 then
     echo "deb http://ppa.launchpad.net/ebox-unstable/ubuntu hardy main" >> $SOURCES
 #    echo "deb http://broken.lan.hq.warp.es/ebox-unstable/hardy ./" >> $SOURCES
-fi    
+fi
+echo "deb http://10.6.7.1/ebox ./" >> $SOURCES
