fd150fa4f2db9e3fdc7aef52bea3cff1fcc40729
José Antonio Calvo <jacalvo@zentyal.com>
2013-11-08 13:58:10 +0100

    fixed previous commit, switching to proper dir

--- src/ANSTE/Comm/SlaveServer.pm
+++ src/ANSTE/Comm/SlaveServer.pm
@@ -55,7 +55,7 @@ sub put # (file, content)
         print $FILE $content;
         close $FILE or die "Can't close: $!";
         if ($name =~ /\.tar$/) {
-            system ("tar xf $DIR/$name");
+            system ("tar xf $DIR/$name -C $DIR");
         }
         return 'OK';
     } else {
